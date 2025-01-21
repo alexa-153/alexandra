546194f783a36bda67657a0ed7802905be11187d
alexa-153 <alexamaningo25@gmail.com>
2025-01-21 11:14:03 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+const projects = [
+    {
+        title: "Project 1",
+        description: "Bio Data.",
+        image: "biodata.jpg"
+    },
+    {
+        title: "OOP SYSTEM",
+        description: "DIFFICULT AMONG OTHERS.",
+        image: "system.jpg"
+    },
+    {
+        title: "SCRATCH",
+        description: "FUN TO CREATE.",
+        image: "scratch.jpg"
+    }
+];
+
+
+function showAlert() {
+    alert('Thank you for visiting my portfolio!');
+}
